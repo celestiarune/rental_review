2e3e544b1897157edccccb690f6c9df1a712b1b5
Noah <celestiarune@gmail.com>
2023-08-28 18:06:39 +0900

    change form to model form

--- my_site/cars/forms.py
+++ my_site/cars/forms.py
@@ -1,7 +1,30 @@
 from django import forms
+from .models import Review
+from django.forms import ModelForm
 
-class ReviewForm(forms.Form):
-    first_name = forms.CharField(label='First Name',max_length=100)
-    last_name = forms.CharField(label='Last Name',max_length=100)
-    email = forms.EmailField(label='Email')
-    review = forms.CharField(label='Please write your review here', widget=forms.Textarea(attrs={'class':'myform','rows':'2','cols':'2'}))
+
+
+# class ReviewForm(forms.Form):
+#     first_name = forms.CharField(label='First Name',max_length=100)
+#     last_name = forms.CharField(label='Last Name',max_length=100)
+#     email = forms.EmailField(label='Email')
+#     review = forms.CharField(label='Please write your review here', widget=forms.Textarea(attrs={'class':'myform','rows':'2','cols':'2'}))
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = "__all__" # pass in all model fields as form fields
+
+        labels = {
+            'first_name':'YOUR FIRST NAME',
+            'last_name':'Last Name',
+            'stars':'Rating:'
+        }
+
+        error_messages = {
+            'stars':{
+                'min_value':'Yo! min value is 1',
+                'max_value':'Yo! max value is 5'
+            }
+        }
